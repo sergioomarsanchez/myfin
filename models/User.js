9c39fa8db93173c95f70b140cb978e9cacb0c097
Sergio Omar Sánchez <sanchez.omar.sergio@gmail.com>
2023-01-14 22:01:06 -0300

    git ignore updated

--- models/User.js
+++ models/User.js
@@ -10,6 +10,7 @@ const userSchema = new mongoose.Schema({
     password:{type:String, required:true}
 })
 
+
 userSchema.methods.generateAuthToken = function(){
 const token = jwt.sign({_id: this._id}, process.env.JWTPRIVATEKEY, {expiresIn: '1d'});
 return token
