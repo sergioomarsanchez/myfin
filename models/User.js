8555b031178ee033bd7bf671acce192c60410fea
Sergio Omar Sánchez <sanchez.omar.sergio@gmail.com>
2023-01-14 21:21:48 -0300

    user model created

---
+++ models/User.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+import jwt from 'jsonwebtoken';
+import Joi from 'joi';
+import passwordComplexity from "joi-password-complexity";
+
+const userSchema = new mongoose.Schema({
+    firstName:{type:String, required:true},
+    lastName:{type:String, required:true},
+    email:{type:String, required:true},
+    password:{type:String, required:true}
+})
+
+userSchema.methods.generateAuthToken = function(){
+const token = jwt.sign({_id: this._id}, process.env.JWTPRIVATEKEY, {expiresIn: '1d'});
+return token
+}
+
+const User = mongoose.model("user", userSchema)
+
+const validate = (data)=>{
+    const schema = joi.object({
+        firstName: Joi.string().required().label('First Name'),
+        lastName: Joi.string().required().label('Last Name'),
+        email: Joi.string().required().label('Email'),
+        password: passwordComplexity.required().label('Password')
+    })
+    return schema.validate(data)
+}
+
+module.exports = {User, validate}
