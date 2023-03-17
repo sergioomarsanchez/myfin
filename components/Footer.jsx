e2c68c79315a43040c697f4dc30b36ef84680d89
Sergio Omar Sánchez <sanchez.omar.sergio@gmail.com>
2023-03-17 19:20:18 -0300

    CSS footer updated

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
 import style from '../styles/Footer.module.css'
 import Image from 'next/image'
+// import facebook from '/img/socialMediaIcons/icons8-facebook-circled-94.png'
 
 function Footer() {
   return (
     <div className={style.container}>
-      <div className={style.img}>
-      <Image src='/img/FooterImg.png' alt='' height={100} width={500} className={style.footerImg}/>
+      <div className={style.socialMediaContainer}>
+        <div className={style.icons}>
+          <a href='https://www.youtube.com/channel/UChIMb1J6ILBRcq-b6PQ7SIQ'>
+          <Image src='/img/socialMediaIcons/icons8-youtube-94.png' alt='' height={50} width={50} className={style.iconImg}/>
+          </a>
+          <a href='https://www.linkedin.com/in/sergio-omar-sanchez-6ba362104'>
+          <Image src='/img/socialMediaIcons/icons8-linkedin-94.png' alt='' height={50} width={50} className={style.iconImg}/>
+          </a>
+          <a href='https://www.instagram.com/sergio_omar_sanchez/'>
+          <Image src='/img/socialMediaIcons/icons8-instagram-94.png' alt='' height={50} width={50} className={style.iconImg}/>
+          </a>
+          <a href='https://github.com/sergioomarsanchez'>
+          <Image src='/img/socialMediaIcons/icons8-github-94.png' alt='' height={50} width={50} className={style.iconImg}/>
+          </a>
+          <a href='mailto:sanchez.omar.sergio@gmail.com'>
+          <Image src='/img/socialMediaIcons/icons8-gmail-logo-94.png' alt='' height={50} width={50} className={style.iconImg}/>
+          </a>
+        </div>
+        <div className={style.iconsSrc}>Icons from <a className={style.iconsLink} href='https://icons8.com/'>icons8.com</a></div>
       </div>
       <div className={style.left}>
         <h4>The best app to manage your finances, savings, investment and budgets</h4>
