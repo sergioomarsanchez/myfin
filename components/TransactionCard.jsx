94244b407480a04587d985522c82a8881a93adbe
Sergio Omar Sánchez <sanchez.omar.sergio@gmail.com>
2023-04-04 13:28:42 -0300

    balances bug updated

--- components/TransactionCard.jsx
+++ components/TransactionCard.jsx
@@ -26,10 +26,10 @@ function TransactionCard({ transId, currentBalance, transaction, entityName, set
 
                     if(transaction.type === 'debit'){
                         currentBalance = currentBalance + Number(transaction.amount)
-                        setBalance(currentBalance)
+                       // setBalance(currentBalance)
                     } else {
                         currentBalance = currentBalance - Number(transaction.amount)
-                        setBalance(currentBalance)
+                       // setBalance(currentBalance)
                     }
                     await axios.put(`https://myfin-sergioomarsanchez.vercel.app/api/accounts/` + transactionAccount, { balance:currentBalance })
                     dispatch(updateAccBalance({id:transactionAccount, newBalance:currentBalance}))
@@ -41,7 +41,7 @@ function TransactionCard({ transId, currentBalance, transaction, entityName, set
                   text:'Done.',
                   icon:'success'})
                   dispatch(deleteTransaction(transactionAccount, id))
-                  dispatch(updateTotals({transactionType:transaction.type, currency: entityName.toLowerCase().includes(' ars')?'ars':'usd', amount:transaction.amount}))
+                  dispatch(updateTotals({transactionType:transaction.type==='credit'?'debit':'credit', currency: entityName.toLowerCase().includes(' ars')?'ars':'usd', amount:transaction.amount}))
                   
                 } catch (error) {
                     if(error.response && error.response.status >=400 && error.response.status <= 500){
