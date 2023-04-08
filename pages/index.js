dc004e60f64ca534e5a35240f41c2dcb5d6c2e5d
Sergio Omar Sánchez <sanchez.omar.sergio@gmail.com>
2023-04-08 17:49:43 -0300

    about responsive added

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>My Fin app</title>
-        <meta name="description" content="Best pizza of Argentina" />
+        <meta name="description" content="Best finances app of Argentina" />
         <link rel="icon" href="/img/LogoDark.png" />
       </Head>
     <HomePage/>
